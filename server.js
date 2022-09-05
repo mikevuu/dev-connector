e4be9ead80eb748c7b22b9dae0fba202f93396ad
unknown <vuuthongnguyen@gmail.com>
2022-09-05 18:39:37 +0700

    3rd commit

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const app = express();
 connectDB();
 
 app.get('/', (req,res) => res.send('API Running'));
+
+// Define Routes
+
+app.use('/api/users', require('./routes/api/users'));
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
